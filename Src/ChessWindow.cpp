ee9cb20508b56538c211adc241b592fb6add823a
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-12-26 00:12:18 +0100

    Src/ChessWindow.cpp: fixed basic GUI

--- Src/ChessWindow.cpp
+++ Src/ChessWindow.cpp
@@ -1,5 +1,7 @@
 #include "../ChessWindow.h"
-
+#ifndef NULL
+#define NULL 0
+#endif
 ChessWindow::ChessWindow()
 {
    clientW = 800;
@@ -31,11 +33,36 @@ struct Animation
    Piece* piece;     // Pointer to the piece to be animated
    int xFrom, yFrom; // Starting position of the animation
    int xEnd, yEnd;   // Ending position of the animation
-   int duration;
+   float duration;
+   float fullDuration;
+   Piece* toKill;
+   Animation(Piece* piece, int xFrom, int yFrom, int xEnd, int yEnd, float duration, Piece* toKill)
+   {
+      this->piece = piece;
+      this->xFrom = xFrom;
+      this->yFrom = yFrom;
+      this->xEnd  = xEnd;
+      this->yEnd  = yEnd;
+      this->duration = duration;
+      this->fullDuration = duration;
+      this->toKill = toKill;
+   }
 };
 void ChessWindow::start()
 {
-   int mouseX, mouseY;
+   CChess::ChessBoard board;               // Board to play chess
+   // Control variables
+   int mouseX, mouseY;                     // Cursor position
+   bool leftMousePressed;                  // Left button pressed
+   bool rightMousePressed;                 // Right button pressed
+   int selectedX, selectedY;               // Selected cell position
+   bool selected = false;                  // Is there a selected piece
+   std::list<CChess::Move> availableMoves; // Moves available to the selected piece
+   std::list<CChess::Move>::const_iterator it;
+   std::list<Animation*> animations;       // Animations to show
+   CChess::Move selectedMove(-1,-1,-1,-1); // Selected move
+   bool playerTurn = true;                 // Is this player's turn?
+
    sf::RenderWindow window(sf::VideoMode(clientW, clientH), "CChess");
    // TODO: handle window creation error
    window.setFramerateLimit(60);
@@ -80,31 +107,32 @@ void ChessWindow::start()
 
    // Instantiate all the pieces *******************************************************************
    // **********************************************************************************************
-   std::list<Piece> pieces;
-   pieces.push_back(Piece(0,  0  ,&pieceSprites[BRook]));
-   pieces.push_back(Piece(100,0  ,&pieceSprites[BKnight]));
-   pieces.push_back(Piece(200,0  ,&pieceSprites[BBishop]));
-   pieces.push_back(Piece(300,0  ,&pieceSprites[BQueen]));
-   pieces.push_back(Piece(400,0  ,&pieceSprites[BKing]));
-   pieces.push_back(Piece(500,0  ,&pieceSprites[BBishop]));
-   pieces.push_back(Piece(600,0  ,&pieceSprites[BKnight]));
-   pieces.push_back(Piece(700,0  ,&pieceSprites[BRook]));
-   pieces.push_back(Piece(0,  700,&pieceSprites[WRook]));
-   pieces.push_back(Piece(100,700,&pieceSprites[WKnight]));
-   pieces.push_back(Piece(200,700,&pieceSprites[WBishop]));
-   pieces.push_back(Piece(300,700,&pieceSprites[WQueen]));
-   pieces.push_back(Piece(400,700,&pieceSprites[WKing]));
-   pieces.push_back(Piece(500,700,&pieceSprites[WBishop]));
-   pieces.push_back(Piece(600,700,&pieceSprites[WKnight]));
-   pieces.push_back(Piece(700,700,&pieceSprites[WRook]));
+   std::list<Piece*> pieces;
+   pieces.push_back(new Piece(0,  0  ,&pieceSprites[BRook]));
+   pieces.push_back(new Piece(100,0  ,&pieceSprites[BKnight]));
+   pieces.push_back(new Piece(200,0  ,&pieceSprites[BBishop]));
+   pieces.push_back(new Piece(300,0  ,&pieceSprites[BQueen]));
+   pieces.push_back(new Piece(400,0  ,&pieceSprites[BKing]));
+   pieces.push_back(new Piece(500,0  ,&pieceSprites[BBishop]));
+   pieces.push_back(new Piece(600,0  ,&pieceSprites[BKnight]));
+   pieces.push_back(new Piece(700,0  ,&pieceSprites[BRook]));
+   pieces.push_back(new Piece(0,  700,&pieceSprites[WRook]));
+   pieces.push_back(new Piece(100,700,&pieceSprites[WKnight]));
+   pieces.push_back(new Piece(200,700,&pieceSprites[WBishop]));
+   pieces.push_back(new Piece(300,700,&pieceSprites[WQueen]));
+   pieces.push_back(new Piece(400,700,&pieceSprites[WKing]));
+   pieces.push_back(new Piece(500,700,&pieceSprites[WBishop]));
+   pieces.push_back(new Piece(600,700,&pieceSprites[WKnight]));
+   pieces.push_back(new Piece(700,700,&pieceSprites[WRook]));
    // Create pawns
    for(int i = 0; i < 8; i++)
    {
-      pieces.push_back(Piece(i*100,100,&pieceSprites[BPawn]));
-      pieces.push_back(Piece(i*100,600,&pieceSprites[WPawn]));
+      pieces.push_back(new Piece(i*100,100,&pieceSprites[BPawn]));
+      pieces.push_back(new Piece(i*100,600,&pieceSprites[WPawn]));
    }
 
    bool updatesAvailable = true;
+   board.computeAvailableMoves(CChess::White);
    while (window.isOpen())
    {
        sf::Event event;
@@ -120,6 +148,13 @@ void ChessWindow::start()
              case(sf::Event::MouseMoved):
                 mouseX = event.mouseMove.x;
                 mouseY = event.mouseMove.y;
+                updatesAvailable = true;
+             break;
+             case(sf::Event::MouseButtonPressed):
+                if(event.mouseButton.button == sf::Mouse::Left)
+                   leftMousePressed = true;
+                else if(event.mouseButton.button == sf::Mouse::Right)
+                   rightMousePressed = true;
                 updatesAvailable = true;
              break;
              case(sf::Event::Resized):
@@ -140,18 +175,177 @@ void ChessWindow::start()
 
           // Draw the background
           window.draw(background);
-          // Draw the pieces
-          std::list<Piece>::const_iterator it = pieces.begin();
-          while(it != pieces.end())
+
+          if(animations.size() == 0)
           {
-             (*it).sprite->setPosition(sf::Vector2f((*it).x,(*it).y));
-             window.draw(*(*it).sprite);
-             it++;
+             // Gameplay ***************************************************************************
+             // ************************************************************************************
+             // It's player's turn
+             // Highlight the square under the cursor
+             sf::RectangleShape shape;
+             int selX = 0, selY = 0;
+             while((++selX)*100 < mouseX); selX--;
+             while((++selY)*100 < mouseY); selY--;
+             shape.setPosition(selX*100.0f,selY*100.0f);
+             shape.setSize(sf::Vector2f(100.0f,100.0f));
+             shape.setFillColor(sf::Color::Green);
+             window.draw(shape);
+             if(rightMousePressed)
+             {
+                selected = false;
+                rightMousePressed = false;
+             }
+             if(leftMousePressed)
+             {
+                if(!selected)
+                {
+                   availableMoves.clear();
+                   it = board.moves.begin();
+                   while(it != board.moves.end())
+                   {
+                      if((*it).xFrom == selX && (*it).yFrom == selY)
+                         availableMoves.push_back(*it);
+                      it++;
+                   }
+                   if(availableMoves.size() > 0)
+                   {
+                      selected = true;
+                      selectedX = selX;
+                      selectedY = selY;
+                   }
+                }
+                else
+                {
+                   // Mouse left button pressed with a piece selected:
+                   // if the pointer is over an available move, just move,
+                   // otherwise deselect the piece
+                   bool moveClicked = false;
+                   it = availableMoves.begin();
+                   int xFrom, yFrom, xTo, yTo;
+                   while(it != availableMoves.end())
+                   {
+                      if((*it).xTo == selX && (*it).yTo == selY)
+                      {
+                         moveClicked = true;
+                         xFrom = (*it).xFrom * 100;
+                         yFrom = (*it).yFrom * 100;
+                         xTo   = (*it).xTo   * 100;
+                         yTo   = (*it).yTo   * 100;
+                         selectedMove = (*it);
+                         break;
+                      }
+                      it++;
+                   }
+                   if(moveClicked)
+                   {
+                      Piece* piece, *toKill = NULL;
+                      // Find piece that has to be moved
+                      std::list<Piece*>::const_iterator pit = pieces.begin();
+                      while(pit != pieces.end())
+                      {
+                         if( (*pit)->x == xFrom && (*pit)->y == yFrom )
+                            piece = *pit;
+                         if( (*pit)->x == xTo && (*pit)->y == yTo )
+                            toKill = *pit;
+                         pit++;
+                      }
+                      // start a new animation
+                      // Find eventual piece to kill
+
+                      animations.push_back(
+                            new Animation(piece,piece->x,piece->y,selX*100,selY*100,10,toKill));
+                      selected = false;
+                   }
+                   else
+                   {
+                      // deselect
+                      selected = false;
+                   }
+                }
+                leftMousePressed = false;
+             }
+
+             // Highlight possible moves & selected piece
+             if(selected)
+             {
+                shape.setPosition(selectedX*100.0f,selectedY*100.0f);
+                shape.setFillColor(sf::Color::Yellow);
+                window.draw(shape);
+                it = availableMoves.begin();
+                while(it != availableMoves.end())
+                {
+                   shape.setFillColor(sf::Color::Magenta);
+                   shape.setPosition((*it).xTo*100.0f,(*it).yTo*100.0f);
+                   window.draw(shape);
+                   it++;
+                }
+             }
+             updatesAvailable = false;
+          }
+          if( animations.size() > 0 )
+          {
+              std::list<Animation*>::iterator ait = animations.begin();
+              while(ait != animations.end())
+              {
+                 Animation* anim = *ait;
+                 float f = anim->duration/anim->fullDuration;
+                 anim->piece->x = (float)anim->xFrom * f + (float)anim->xEnd * (1.0f-f);
+                 anim->piece->y = (float)anim->yFrom * f + (float)anim->yEnd * (1.0f-f);
+                 anim->duration -= 1.0f;
+                 if(anim->duration <= 0.0f)
+                 {
+                    anim->piece->x = anim->xEnd;
+                    anim->piece->y = anim->yEnd;
+                    if(anim->toKill != NULL)
+                    {
+                       pieces.remove(anim->toKill);
+                       delete anim->toKill;
+                    }
+                    // Remove animation from the list
+                    animations.clear();
+                    // Make the move!
+                    board.makeMove(selectedMove);
+                    if(playerTurn)
+                    {
+                       // This is time for the PC to make its move
+                       playerTurn = false;
+                       selectedMove = board.computeBestMove(CChess::Black);
+                       // Add new animation for the move
+                       Piece* piece, *toKill = NULL;
+                       std::list<Piece*>::const_iterator pit = pieces.begin();
+                       while(pit != pieces.end())
+                       {
+                          Piece* pc = *pit;
+                          if(pc->x == selectedMove.xFrom*100 && pc->y == selectedMove.yFrom*100)
+                             piece = pc;
+                          if(pc->x == selectedMove.xTo*100 && pc->y == selectedMove.yTo*100)
+                             toKill = pc;
+                          pit++;
+                       }
+                       animations.push_back(new Animation(piece,piece->x,piece->y,
+                             selectedMove.xTo*100, selectedMove.yTo*100, 10, toKill));
+                       break;
+                    }
+                    else
+                    {
+                       playerTurn = true;
+                       board.computeAvailableMoves(CChess::White);
+                       break;
+                    }
+                 }
+                 ait++;
+              }
+          }
+          // Draw the pieces
+          std::list<Piece*>::const_iterator pit = pieces.begin();
+          while(pit != pieces.end())
+          {
+             (*pit)->sprite->setPosition(sf::Vector2f((*pit)->x,(*pit)->y));
+             window.draw(*(*pit)->sprite);
+             pit++;
           }
 
           window.display();
-
-          updatesAvailable = false;
-       }
+       } // if(updatesAvailable)
    }
 }
