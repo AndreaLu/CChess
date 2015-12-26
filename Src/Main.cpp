a76e3e0bc40be798bbca0bd36a2cceb765bb3b1c
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-12-26 18:35:21 +0100

    Src/Main.cpp: code ordered

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,6 +1,6 @@
 #include "../CChess.h"
 #include "../ChessWindow.h"
-#define GAMEGUI 0 // 1 : GUI, 0 : CONSOLE
+#define GAMEGUI 1 // 1 : GUI, 0 : CONSOLE
 
 #include <string>
 #include <sstream>
@@ -16,59 +16,54 @@ int main()
    ChessWindow window;
    window.start();
 #else
+   // Console play, init some variables
    CChess::ChessBoard board;
-   // Console play
    std::string input;
+   bool invalidInput = true;
+   int coordinates[] = {0,0,0,0};
+
    while( true )
    {
       std::cout << board.getString() << std::endl << "Your turn! input like 'xFrom,yFrom,xTo,yTo'"
                 << std::endl;
 
-
-      bool invalidInput = true;
-      int coordinates[] = {0,0,0,0};
       while(invalidInput)
       {
-         std::cin  >> input;
+         // Acquire input from console
+         std::cin >> input;
          std::vector<std::string> values = split(input,',');
+
          // Verify the input is valid **************************************************************
          // ****************************************************************************************
+
+         // Verify the user typed 4 values separated by 3 commas
          if(values.size() != 4)
          {
             std::cout << "Please type 4 values." << std::endl;
             continue;
          }
+
+         // Verify the values were numbers
+         bool validCoordinates = true;
          for(int i = 0; i < 4; i++)
-         {
-            if ( ! (std::istringstream(values[i]) >> coordinates[i]) )
+            if ( !(std::istringstream(values[i]) >> coordinates[i]) )
             {
                std::cout << values[i] << " is not a valid coordinate." << std::endl;
+               validCoordinates = false;
                break;
             }
-            if(coordinates[i] < 0)
-            {
-               std::cout << "A coordinate cannot be < 0." << std::endl;
-               break;
-            }
-            if(coordinates[i] > 7)
-            {
-               std::cout << "A coordinate cannot be > 7." << std::endl;
-               break;
-            }
-            if(i == 3)
-               invalidInput = false;
-         }
-         if(invalidInput)
+         if( !validCoordinates )
             continue;
-         invalidInput = true;
-         board.computeAvailableMoves(CChess::White);
+
+         // Verify the numbers make up a valid move
          std::list<CChess::Move>::const_iterator mit = board.moves.begin();
          while(mit != board.moves.end())
          {
-            if((*mit).xFrom == coordinates[0] &&
-               (*mit).yFrom == coordinates[1] &&
-               (*mit).xTo   == coordinates[2] &&
-               (*mit).yTo   == coordinates[3])
+            CChess::Move move = *mit;
+            if( move.xFrom == coordinates[0] &&
+                move.yFrom == coordinates[1] &&
+                move.xTo   == coordinates[2] &&
+                move.yTo   == coordinates[3])
             {
                invalidInput = false;
                break;
@@ -78,13 +73,13 @@ int main()
          if(invalidInput)
             std::cout << "The specified move is invalid." << std::endl;
       }
+
       // The specified move is valid ***************************************************************
       // *******************************************************************************************
       board.makeMove(CChess::Move(coordinates[0],coordinates[1],coordinates[2],coordinates[3]));
       // TODO: detect player's victory
       std::cout << board.getString() << std::endl << "My turn, lemme think..." << std::endl;
-      CChess::Move bestMove = board.computeBestMove(CChess::Black);
-      board.makeMove(bestMove);
+      board.makeMove(board.computeBestMove(CChess::Black));
       // TODO: detect computer's victory
    }
 #endif
