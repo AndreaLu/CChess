211c8aa15bde669fcba67e03707debded10e3a2e
Andrea Luzzati <andrea.luzzati@gmail.com>
2015-12-27 19:27:09 +0100

    Added == operator for Piece

--- Piece.h
+++ Piece.h
@@ -5,12 +5,16 @@ namespace CChess
 {
    struct Piece
    {
-
       enum Type {None, Pawn, Knight, Bishop, Rook, Queen, King } type;
       Player owner;
+      bool operator==(const Piece& piece)
+      {
+         return( owner == piece.owner && type == piece.type );
+      }
       Piece()
       {
          type = None;
+         owner = Black;
       }
       Piece(Type type, Player owner)
       {
